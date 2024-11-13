d2298b633cd78287af546c93fb50cd9fa82eecb7
Adil Muratov <muratowadil@gmail.com>
2024-11-13 15:13:14 +0300

    second update

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+#CHARACTERISTICS OF WINDOW
+root.title("Help for martians")
+root.geometry("500x400+250+100")
+root.resizable(False, False)
+
+#LABELS
+
+
+#BUTTONS
+
+#ENTRIES
+
+#PLACEMENT OF OBJECTS
+
+
+root.mainloop()
